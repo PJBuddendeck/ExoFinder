de3a4f98dffac2b8ba523a10b9c1742b3af0df97
Peter Joseph Buddendeck <pjbuddendeck@wm.edu>
2026-03-21 18:44:11 -0400

    Extended footer shadow to cover remainder of screen when screen content is left unfilled

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -4,7 +4,8 @@ import '../style.css';
 function App() {
   return (
     <footer className="footer-container">
-      <p>More Info to Come!</p>
+      <p>This website has made use of the NASA Exoplanet Archive, which is operated by the California Institute of Technology, 
+        under contract with the National Aeronautics and Space Administration under the Exoplanet Exploration Program.</p>
     </footer>
   );
 }
